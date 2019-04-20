188c3fc88cd9972118f96d88a31382000ac923c4
Martin Safranek <martinsafranek1997@seznam.cz>
2019-04-20 16:28:24 +0200

    import

--- reference/bi-tjv-c-11-vychozi/RestClient/src/main/java/cz/cvut/fit/tjv/cv/tvseries/rest_client/SeriesServiceException.java
+++
@@ -1,7 +0,0 @@
-package cz.cvut.fit.tjv.cv.tvseries.rest_client;
-
-public class SeriesServiceException extends RuntimeException {
-    public SeriesServiceException(Throwable cause) {
-        super(cause);
-    }
-}
